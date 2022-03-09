fb526ee23c02a94dafb0df406d278fc8d981d3ac
Calvin <calvin.nel00@gmail.com>
2022-03-09 13:21:06 +0200

    Navigation screen properly setup with home,schools,teachers, stil need to put in notifications.

---
+++ app/src/main/java/com/example/myapplication/ui/schools/SchoolsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.ui.schools
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SchoolsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is schools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/example/myapplication/ui/teachers/TeachersFragment.kt
@@ -0,0 +1,44 @@
+package com.example.myapplication.ui.teachers
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.databinding.FragmentTeachersBinding
+import com.example.myapplication.ui.schools.TeachersViewModel
+
+
+class TeachersFragment : Fragment() {
+
+    private var _binding: FragmentTeachersBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+            ViewModelProvider(this).get(TeachersViewModel::class.java)
+
+        _binding = FragmentTeachersBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textTeachers
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
